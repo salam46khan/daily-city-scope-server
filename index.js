821ca3ad06b84e68106570cf3def3c67d495197e
salam46khan <salam46.khan@gmail.com>
2023-12-04 04:03:43 +0600

    make admin api

--- index.js
+++ index.js
@@ -100,6 +100,12 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/mynews/:id', async (req, res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result =await newsCollection.deleteOne(query)
+            res.send(result)
+        })
 
         // user collection 
         app.post('/users', async (req, res)=>{
@@ -151,6 +157,19 @@ async function run() {
             }
             const cursor = userCollection.find(query)
             const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.patch('/user/admin/:id', async (req, res)=>{
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)};
+            const options = {upsert: true};
+            const update = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter,update, options)
             res.send(result)
         })
 
